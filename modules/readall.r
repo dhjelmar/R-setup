a1a2f7aa342ed7360d057003ca5291a362547602
dhjelmar <dlhjel@gmail.com>
2021-06-18 10:12:31 -0400

    changed input parameters for readall.r

--- modules/readall.r
+++ modules/readall.r
@@ -1,16 +1,19 @@
 readall <- function(datasource,
-                    header=TRUE,      # row where header starts
-                    nheader=1,        # number of header rows (all but first will be skipped)
-                    datastartrow=2,   # row where data starts
-                    skip=0,           # number of lines to skip before reading data
-                    row.names=NULL,   # vector, column number, or column header
-                    sheet=NULL,       # Excel only: sheet name in quotes; defaults to 1st sheet
-                    range=NULL,       # Excel only: cell range in quotes including header; overrides headerrow and datastart row
-                    rename=TRUE,      # If true, change special characters and spaces to "_"
-                    suppress=FALSE    # default prints output of variable names; set to true to suppress output
+                    header.row=1,        # row number of the header; header.row=0 means no header
+                    data.start.row=NULL, # row where data starts; default is header.row + 1
+                    data.end.row=NULL,   # Excel only: row where data ends; default is end of file
+                    sheet=NULL,          # Excel only: sheet name in quotes; defaults to 1st sheet
+                    range=NULL,          # Excel only: cell range in quotes including 1 line header; overrides header.row and data.start.row
+                    col.names=NULL,      # Used to replace column names
+                    rename=TRUE,         # If true, change special characters and spaces in read names to "_";  not used if col.names is specified
+                    suppress=FALSE       # default prints output of variable names; set to true to suppress output
                     ) {
-    ## e.g.,: readall("mydata.xlsx", sheet="Test Geometry", range="b3:x99")
 
+    ## e.g.,: readall("mydata.xlsx", sheet="Test Geometry", range="b3:x99")
+    ## e.g.,: readall("mydata.xlsx", sheet="Test Geometry", header.row=4, data.start.row=6)
+
+    if (is.null(data.start.row)) data.start.row <- header.row + 1
+    
     ## datasource <- "test.xlsx"
 
     ## determine if datasource is actually a data table rather than a file
@@ -25,28 +28,70 @@ readall <- function(datasource,
         df <- read.table(text=datasource, header=TRUE)
         datasourcename <- 'internal table'
     } else if (ext == 'csv') {
-        if (nheader == 1) {
-            df <- read.table(datasource, header=TRUE, sep=",", skip=skip)
+        if (header.row == 0) {
+            ## read data; no header
+            df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1)
         } else {
-            hd <- read.table(datasource, header=TRUE,  sep=",", skip=skip,         nrows=nheader)
-            df <- read.table(datasource, header=FALSE, sep=",", skip=skip+nheader)
+            ## read header
+            hd <- read.table(datasource, header=TRUE,  sep=",", skip=header.row-1,    nrows=nheader)
+            ## read data
+            df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1)
+            ## assign header names to columns
             names(df) <- colnames(hd)
         }
         datasourcename <- datasource
+
     } else if (ext == 'xls' | ext == 'xlsx') {
-        if (nheader == 1) {
-            ## df <- read_excel(datasource, col_names=TRUE,       skip=skip,         row.names=row.names)
-            if (skip > 0) {
-                df <- read_excel(datasource, sheet=sheet, col_names=TRUE,       skip=skip)
-            } else if (!is.null(range)) {
-                df <- read_excel(datasource, sheet=sheet, col_names=TRUE,       range=range)
+        if (!is.null(range)) {
+            ## range is defined so this takes precedence
+            if (header.row !=0) {
+                ## simple structure with a single header row followed by data in the provided range
+                df <- read_excel(datasource, sheet=sheet, col_names=TRUE,  range=range)
             } else {
-                df <- read_excel(datasource, sheet=sheet, col_names=TRUE)
-            }                
+                ## no header provided
+                df <- read_excel(datasource, sheet=sheet, col_names=FALSE, range=range)
+            }
+
         } else {
-            hd <- read_excel(datasource, sheet=sheet, col_names=TRUE,       skip=skip,         n_max=nheader, range=range)
-            df <- read_excel(datasource, sheet=sheet, col_names=FALSE,      skip=skip+nheader               , range=range)
-            names(df) <- colnames(hd)
+            if (!is.null(data.end.row)) {
+                ## set max number of rows to read
+                n_max <- data.end.row - data.start.row + 1
+            } else {
+                ## use default to read through the end of the file
+                n_max <- NULL
+            }
+            if (header.row == 0) {
+                ## no header row supplied
+                if (is.null(n_max)) {
+                    ## read to end of file
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1)
+                } else {
+                    ## read specified number of rows
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, n_max=n_max)
+                }                    
+            } else if (data.start.row == header.row + 1) {
+                ## data immediately follows single row header so read both together
+                if (is.null(n_max)) {
+                    ## read to end of file
+                    df <- read_excel(datasource, sheet=sheet, col_names=TRUE, skip=header.row-1)
+                } else {
+                    ## add 1 to n_max to read header plus specified number of data rows
+                    df <- read_excel(datasource, sheet=sheet, col_names=TRUE, skip=header.row-1, n_max=n_max+1)
+                }
+            } else {
+                ## lines between header row and data
+                ## read header
+                hd <- read_excel(datasource, sheet=sheet, col_names=TRUE,  skip=header.row-1, n_max=1)
+                if (is.null(n_max)) {
+                    ## read data to end of file
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1)
+                } else {
+                    ## read specified number of data rows
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, n_max=n_max)
+                }                    
+                ## assign header names to columns
+                names(df) <- colnames(hd)
+            }                
         }
         datasourcename <- datasource
     } else {
@@ -65,6 +110,8 @@ readall <- function(datasource,
     ## use regular expression to replace space, parenthesis, /, +, -, ., and comma with _
     if (rename == TRUE) names(df) <- gsub(" |\\(|\\)|\\/|\\+|\\-|\\.|\\,", "_", names(df))
 
+    if (!is.null(col.names)) names(df) <- col.names
+    
     if (suppress == FALSE) {
         cat('Output dataframe names:\n')
         print(names(df))
@@ -82,4 +129,8 @@ readall <- function(datasource,
 ## 2 	75 	10 	5 	1580 	29 	26 
 ## 3 	55 	25 	5 	1580 	29 	20 
 ## "
-## df <- read.table(text=data, header=TRUE)
+## df <- read.table(text=mydata, header=TRUE)
+## df <- readall(mydata, suppress=TRUE)
+## df <- readall('test.xlsx', header.row=2, data.end.row=5)
+## df <- readall('test.xlsx', range='b2:d11')
+## readall('readall.xlsx', range='b2:e5', col.names=c('fred', 'Ethyl Mertz', 'lucy', 'desi'))
