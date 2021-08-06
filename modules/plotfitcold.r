4a8787d141b7abfbe672ce84f794fecd69bc7bd6
dlhjel <dlhjel@gmail.com>
2021-08-06 15:09:41 -0400

    fixed equation title and added vlines and xlimspec input parameters

--- modules/plotfitcold.r
+++ modules/plotfitcold.r
@@ -1,4 +1,7 @@
-plotfitcold <- function(df,xx,yy,byvar,xlimspec=NULL,ylimspec=NULL,bg="grey90",outputfile=NULL) {
+plotfitcold <- function(df,xx,yy,byvar,xlimspec=NULL,ylimspec=NULL, vlines=NULL,
+                        main = 'equation', xlabel = NULL, ylabel = NULL,
+                        interval='conf', alpha=0.05, sided=2, bg="grey90",
+                        suppress='no', outputfile=NULL) {
     ## create scatter plot with linear regression line and 95% confidence lines
     ## xx and yy are variables within dataframe df
     ## color datapoints by DISCRETE variable byvar  <-- This is the distinction from plotfitcol
@@ -36,20 +39,31 @@ plotfitcold <- function(df,xx,yy,byvar,xlimspec=NULL,ylimspec=NULL,bg="grey90",o
     ## put xx, yy, and byvar into dataframe
     newdf <- data.frame(xx,yy,byvar)
 
-    ## perform regression
-    fit       <- lm(yy~xx,data=newdf)
-    intercept <- fit$coefficients[[1]]
-    slope     <- fit$coefficients[[2]]
-    eq = paste0("y = ", signif(intercept,4), "* x + ", signif(slope,4))
-
     ## determine x and y range for plot
-    xmin <- min(newdf$xx,na.rm=TRUE)
-    xmax <- max(newdf$xx,na.rm=TRUE)
+    xmin <- min(newdf$xx, xlimspec, na.rm=TRUE)
+    xmax <- max(newdf$xx, xlimspec, na.rm=TRUE)
     xmax_plot <- xmax + (xmax-xmin)*0.2  # did this to make room for a legend
     ymin <- min(newdf$yy,na.rm=TRUE)
     ymax <- max(newdf$yy,na.rm=TRUE)
     if (missing(xlimspec)) { xlimspec <- c(xmin,xmax_plot) }
     if (missing(ylimspec)) { ylimspec <- c(ymin,ymax) }
+
+    ## perform regression
+    fit       <- lm(yy~xx,data=newdf)
+    intercept <- fit$coefficients[[1]]
+    slope     <- fit$coefficients[[2]]
+    ## calculate rise of fit over range bounds if supplied, otherwise calculate rise over range of data
+    if ( !is.null(vlines) ) {
+        rise <- (vlines[2] - vlines[1]) * slope
+    } else {
+        rise <- (xmax - xmin) * slope
+    }        
+
+    ## plot points and fit
+    if (main == 'equation') main = paste0("y = ", signif(slope,4), "* x + ", signif(intercept,4),
+                                          ", ", expression(Delta), " = ", signif(rise,4))
+
+    ## setup for output jpeg file
     if (!missing(outputfile)) jpeg(filename=outputfile)
 
     ## add color definition for each point using byvar
@@ -69,19 +83,29 @@ plotfitcold <- function(df,xx,yy,byvar,xlimspec=NULL,ylimspec=NULL,bg="grey90",o
 
     ## create plot of points
     par(bg=bg)  # changes background of plot to specified color
-    plot(newdf$xx,newdf$yy,xlim=xlimspec,ylim=ylimspec,
-         xlab=xlabel,ylab=ylabel,main=eq,
+    plot(newdf$xx, newdf$yy, xlim=xlimspec, ylim=ylimspec,
+         xlab=xlabel, ylab=ylabel, main=main,
          col=newdf$color)
     grid(col='gray70')
 
     ## add fit to plot
-    new.xx <- seq(min(newdf$xx,na.rm=TRUE),max(newdf$xx,na.rm=TRUE),len=100)
+    new.xx <- seq(min(newdf$xx, vlines, na.rm=TRUE),max(newdf$xx, vlines, na.rm=TRUE), len=100)
     fit    <- lm(yy~xx,data=newdf)
-    pred   <- predict(fit, new=data.frame(xx=new.xx), interval="conf", level=1-0.05)
+    pred   <- predict(fit, new=data.frame(xx=new.xx), interval=interval, level=1-0.05/sided)
     lines(new.xx,pred[,"fit"],lwd=2)
-    lines(new.xx,pred[,"lwr"],lty=3)
-    lines(new.xx,pred[,"upr"],lty=3)
 
+    ## if request confidence bounds, add to plot
+    if (interval != 'none') {
+        lines(new.xx,pred[,"lwr"],lty=3)
+        lines(new.xx,pred[,"upr"],lty=3)
+    }
+
+    ## if request upper/lower range bounds, add to plot
+    if ( !is.null(vlines) ) {
+        abline(v=vlines[1], col='black', lty='dashed')
+        abline(v=vlines[2], col='black', lty='dashed')
+    }        
+    
     ## add legend
     legendcolor <- unique(newdf$color)
     legendlabel <- unique(newdf$byvar)
@@ -89,14 +113,14 @@ plotfitcold <- function(df,xx,yy,byvar,xlimspec=NULL,ylimspec=NULL,bg="grey90",o
     ##legend("bottomright",title=bylabel,col=newdf$byvar, pch=19,legend=legendlabel)
     legend("bottomright", title=bylabel, col=legendcolor, pch=19, legend=legendlabel)
 
-    ## four output file
+    ## for output file
     if (!missing(outputfile)) dev.off()
 
     ## print results of fit
     estbound(fit)
 
-    ## return variables for debugging
-    return(list( newdf=newdf, cols=cols, legendcolor=legendcolor, legendlabel=legendlabel, fit=fit))
+    if (suppress == 'no')
+        return( list(intercept = intercept, slope = slope, rise = rise, pred = as_tibble(pred)) )
 }
 ## example: plotfitcold(df,gnom,average_g_ts,TS_ID,bg="white")
 ##          plotfitcold(mtcars, disp, mpg, byvar=cyl)
