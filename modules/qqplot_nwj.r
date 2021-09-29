9ab17c225690f61fd683f620164a94c07a7f6c95
dlhjel <dlhjel@gmail.com>
2021-09-29 15:46:33 -0400

    changed to use tolerance rather than qualityTools package to determine Weibull parameters since seems to be more robust and more consistent with hist_nwj.r

--- modules/qqplot_nwj.r
+++ modules/qqplot_nwj.r
@@ -17,7 +17,14 @@ qqplot_nwj <- function(x, type='nwj', jfit='all', mainadder=NULL) {
     if (grepl('w', type) & min(x)>0) {        
         ## make Weibull QQ plot
         main <- paste('Weibull QQ Plot', mainadder, sep=" ")
-        wparms <- qualityTools::qqPlot(x, "Weibull", col='black', main=main)
+        ## tolerance package seems more robust than qualityTools
+        ##     wparms <- qualityTools::qqPlot(x, "Weibull", col='black', main=main)
+        ## also, tolerance packages is used in hist_nwj so more consistent
+        out     <- tolerance::exttol.int(x, alpha=0.05, P=0.95, side=1, dist='Weibull')
+        shape   <- out$'shape.1'
+        scale   <- out$'shape.2'
+        wparms  <- list(shape=shape, scale=scale)
+        qualityTools::qqPlot(x, "Weibull", col='black', main=main, start=wparms)
     } else if (grepl('w', type)) {
         ## some values are negative or zero so Weibull is not appropriate
         cat('Weibull plot not made because not appropriate; some values not > 0\n')
@@ -84,45 +91,31 @@ qqplot_nwj <- function(x, type='nwj', jfit='all', mainadder=NULL) {
     return(list(nparms=nparms, wparms=wparms, jparms=jparms))
     
 }
-## qqplot_nwj(mtcars$mpg)
 
 
-## ## comparisons for normal qq plot
-## par(mfrow=c(1,2))
-## qualityTools::qqPlot(x, "normal",  col='black')
-## quantiles_norm <- qnorm(ppoints(length(x)), mean = mean(x), sd = sd(x))
-## stats::qqplot(
-##            x = x,
-##            xlab = "Data",
-##            y = quantiles_norm,
-##            ylab = 'Quantiles from Normal Distribution',
-##            main = expression('Q-Q plot for Normal Distribution'),
-##            col='black')
-## qqline(x, distribution = function(p) qnorm(p, mean=mean(x), sd=sd(x)), col=2)
-## 
-## ## comparisons for Weibull qq plot
-## par(mfrow=c(1,2))
-## qualityTools::qqPlot(x, "Weibull",  col='black')
-## tol_out <-  exttol.int(x, alpha =0.1, P=0.99, side=1)
-## shape <- tol_out$'shape.1'
-## scale <- tol_out$'shape.2'
-## quantiles_weib <- qweibull(ppoints(length(x)), shape = shape, scale = scale)
-## stats::qqplot(
-##            x = x,
-##            xlab = "Data",
-##            y = quantiles_weib,
-##            ylab = 'Quantiles from Weibull Distribution',
-##            main = expression('Q-Q plot for Weibull Distribution'),
-##            col='black')
-## qqline(x, distribution = function(p) qweibull(p, shape = shape, scale = scale), col=2)
 
-## ## test method of using qJohnson by using same method for qnorm
-## plotspace(1,2)
-## x <- sort(mtcars$mpg)
-## ## by hand
-## probabilities <- ppoints(length(x))
-## xtheoretical  <- qnorm(probabilities, mean=mean(x), sd=sd(x))
-## plot(x, xtheoretical, xlab='Observed value, x', ylab='Expected Value')
-## abline(0,1,col='red')
-## ## using Rfunction
-## qualityTools::qqPlot(x)
+##-----------------------------------------------------------------------------
+qqplot_nwj_tests <- function() {
+
+    qqplot_nwj(mtcars$mpg)
+
+    x <- sort(mtcars$mpg)
+
+    ## comparisons for normal qq plot using same method as for Johnson qqplot
+    par(mfrow=c(1,2))
+    qualityTools::qqPlot(x, "normal",  col='black')
+    xtheoretical <- qnorm(ppoints(length(x)), mean = mean(x), sd = sd(x))
+    plot(x, xtheoretical, xlab='Observed value, x', ylab='Expected Value')
+    abline(0,1, col='red')
+    
+    ## comparisons for Weibull qq plot using same method as for Johnson qqplot
+    par(mfrow=c(1,2))
+    qualityTools::qqPlot(x, "Weibull",  col='black')
+    tol_out <-  exttol.int(x, alpha =0.1, P=0.99, side=1)
+    shape <- tol_out$'shape.1'
+    scale <- tol_out$'shape.2'
+    xtheoretical <- qweibull(ppoints(length(x)), shape = shape, scale = scale)
+    plot(x, xtheoretical, xlab='Observed value, x', ylab='Expected Value')
+    abline(0,1, col='red')
+
+}
