fd19f079a73379d7d02ecb1b77093c211ff1963c
dlhjel <dlhjel@gmail.com>
2021-10-15 10:07:11 -0400

    pulled out of plotdf.r

--- modules/addfit.r
+++ modules/addfit.r
@@ -1,4 +1,4 @@
-addfit <- function(xx, yy, col='black', interval='conf', sided=2, pch=1, vlines=NULL) {
+addfit <- function(xx, yy, col='black', interval='conf', alpha=0.05, sided=2, pch=1, vlines=NULL) {
     ## plot points
     points(xx, yy, pch=pch, col=col)
 
@@ -8,17 +8,17 @@ addfit <- function(xx, yy, col='black', interval='conf', sided=2, pch=1, vlines=
     slope     <- fit$coefficients[[2]]
 
     ## add vertical lines to plot
-    if (!is.null(vlines[1])) abline(v=vlines, lty=3, col=col)
+    if ( !is.null(vlines[1]) & !is.na(vlines[1]) ) abline(v=vlines, lty=3, col=col)
 
     ## add prediction to plot
-    if (!is.null(vlines[1])) {
+    if ( !is.null(vlines[1]) & !is.na(vlines[1]) ) {
         ## restrict fit to within vlines
         new.xx <- seq(min(vlines, na.rm=TRUE), max(vlines, na.rm=TRUE), len=100)
     } else {
         ## extend data to range of data
         new.xx <- seq(min(xx, na.rm=TRUE), max(xx, na.rm=TRUE), len=100)
     }
-    pred   <- predict(fit, new=data.frame(xx=new.xx), interval=interval, level=1-0.05/sided)
+    pred   <- predict(fit, new=data.frame(xx=new.xx), interval=interval, level=1-alpha/sided)
     lines(new.xx, pred[,"fit"], lwd=2, col=col)
     lines(new.xx, pred[,"lwr"], lty=3, col=col)
     lines(new.xx, pred[,"upr"], lty=3, col=col)
@@ -28,7 +28,7 @@ addfit <- function(xx, yy, col='black', interval='conf', sided=2, pch=1, vlines=
 
     ## calculate rise of fit over range bounds if supplied,
     ## otherwise calculate rise over range of data
-    if ( !is.null(vlines[1]) ) {
+    if ( !is.null(vlines[1]) & !is.na(vlines[1]) ) {
         rise <- (vlines[2] - vlines[1]) * slope
     } else {
         rise <- (max(xx) - min(xx)) * slope
