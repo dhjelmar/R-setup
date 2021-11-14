51eee42b2155877e3d150ac967e750cb9b0ffaef
dhjelmar <dlhjel@gmail.com>
2021-11-13 19:50:43 -0500

    added interval options for mean (no conf bounds) and line segment

--- modules/addfit.r
+++ modules/addfit.r
@@ -25,11 +25,21 @@ addfit <- function(xx, yy, col='black', interval='conf', alpha=0.05, sided=2, pc
         ## extend data to range of data
         new.xx <- seq(min(xx, na.rm=TRUE), max(xx, na.rm=TRUE), len=100)
     }
-    pred   <- predict(fit, new=data.frame(xx=new.xx), interval=interval, level=1-alpha/sided)
-    lines(new.xx, pred[,"fit"], lwd=2, col=col)
-    lines(new.xx, pred[,"lwr"], lty=3, col=col)
-    lines(new.xx, pred[,"upr"], lty=3, col=col)
-
+    if (interval == 'mean') {
+        lines(new.xx, slope * new.xx + intercept, lwd=2, col=col)
+    } else if (interval == 'line') {
+        newdf <- data.frame(xx,yy)
+        newdf <- newdf[order(newdf$xx),]
+        xx <- newdf$xx
+        yy <- newdf$yy
+        lines(xx, yy, lwd=2, col=col)       
+    } else {
+        pred   <- predict(fit, new=data.frame(xx=new.xx), interval=interval, level=1-alpha/sided)
+        lines(new.xx, pred[,"fit"], lwd=2, col=col)
+        lines(new.xx, pred[,"lwr"], lty=3, col=col)
+        lines(new.xx, pred[,"upr"], lty=3, col=col)
+    }
+        
     ## print results of fit
     ## estbound(fit)
 
