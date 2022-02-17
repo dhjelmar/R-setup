38b7d2a4dcd8ec65211327ae991b8628c6c62d63
dlhjel <dlhjel@gmail.com>
2022-02-16 21:08:49 -0500

    1st version that I think correctly calculations JohnsonSU tolerance limit using maximum likelihood approach

--- modules/johnson_tol_dan.r
+++ modules/johnson_tol_dan.r
@@ -1,4 +1,4 @@
-johnson_tol <- function(x, jfit='all', alpha=0.01, P=0.99, side=1, plots=FALSE, breaks=NULL) {
+johnson_tol <- function(x, jfit='all', alpha=0.01, P=0.99, side=1, plots=FALSE, plot.ll=FALSE, breaks=NULL) {
 
     ## input:   x     = data set
     ##          alpha = 1 - confidence
@@ -19,7 +19,7 @@ johnson_tol <- function(x, jfit='all', alpha=0.01, P=0.99, side=1, plots=FALSE, 
     ##          xtol_lower = lower bound tolerance limit
     ##          xtol_upper = upper bound tolerance limit
     
-    proportion <- P
+    coverage <- P
 
     ##--------------------------------------------------------
     ## initialize tolerance limits
@@ -57,26 +57,225 @@ johnson_tol <- function(x, jfit='all', alpha=0.01, P=0.99, side=1, plots=FALSE, 
     
     ##--------------------------------------------------------
     ## Calculate tolerance limits if JohnsonSU fit
+    jparms.optim <- NA
+    jparms.q     <- NA
     if (type == 'SU') {
 
-        ## transform x to normal distribution, xn
-        ## https://www.sigmamagic.com/blogs/how-do-i-transform-data-to-normal-distribution/
-        xn <- gamma + delta * asinh( (x - xi)/ lambda )
-        df <- data.frame(x, xn)
+        ##----------------------
+        ## first check to see what parameters an MLE fit gives
+
+        ## define function to return the nnl (negative log likelihood) for
+        ## a given data set and JohnsonSU parameters
+        nll.johnsonsu <- function(x, jparms, debug=FALSE) {
+            ## x is the name of the variable containing the data to be fitted
+            ## if access nnl.johnsonsu with optim, cannot access parameters within
+            ## jparms (e.g., jparms$gamma) with $ because jparms is passed in as atomic vector
+            gamma <- jparms[[1]]  
+            delta <- jparms[[2]]
+            xi    <- jparms[[3]]
+            lambda <- jparms[[4]]
+            ## PDF for Johnson SU
+            pdf <- delta /( lambda * sqrt(2 * pi)   ) *
+                1 / sqrt(1 +            ( (x-xi)/lambda )^2)  *
+                exp( -0.5*(gamma + delta * asinh( (x-xi)/lambda ))^2 )
+            -sum(log(pdf))
+            ## the following is equivalent to the above as long as it picks Johnson SU
+            ## type <- 'SU'
+            ## -sum(SuppDists::dJohnson(x, parms=c(gamma, delta, xi, lambda, type), log=TRUE))
+            if (isFALSE(debug)) {
+                return( -sum(log(pdf)) )
+            } else {
+                return(list(gamma=gamma, delta=delta, xi=xi, lambda=lambda, pdf=pdf))
+            }
+        }
+
+        ## find the JohnsonSU parameters that minimize the nnl
+        out.optim <- optim(par = jparms[1:4], # parameters to be optimized and initial guess
+                           fn  = nll.johnsonsu,     # NNL function
+                           x   = x,                 # data
+                           control = list(parscale = jparms[1:4]) # not clear this was needed
+                           )
+        jparms.optim <- as.list(out.optim$par)
+        jparms.optim$type <- 'SU'
+
+        ##----------------------
+        ## define function to return the nnl (negative log likelihood) for
+        ## an alternate set and JohnsonSU parameters where replace gamma with quantile, i.e.:
+        ##      quantile, delta, xi, lambda
+        ## instead of:
+        ##      gamma,    delta, xi, lambda
+        nll.johnsonsu.quant <- function(x, coverage, jparms, debug=FALSE) {
+            ## x is the name of the variable containing the data to be fitted
+            ## coverage is the percent coverage
+            ## if use optim, cannot access as jparms$gamma because
+            ## jparms is passed in as atomic vector
+            quant  <- jparms[[1]]  # replaced gamma with quant as a parameter
+            delta  <- jparms[[2]]
+            xi     <- jparms[[3]]
+            lambda <- jparms[[4]]
+            ## write gamma as a function of quant, delta, xi and lambda
+            gamma <- qnorm(coverage) - delta * asinh( (quant-xi)/lambda )
+            ## PDF for Johnson SU
+            pdf <- delta /( lambda * sqrt(2 * pi)   ) *
+                1 / sqrt(1 +            ( (x-xi)/lambda )^2)  *
+                exp( -0.5*(gamma + delta * asinh( (x-xi)/lambda ))^2 )
+            if (isFALSE(debug)) {
+                return( -sum(log(pdf)) )
+            } else {
+                return(list(quant=quant, gamma=gamma, delta=delta, xi=xi, lambda=lambda, pdf=pdf))
+            }
+        }
+
+        ## find the alternate JohnsonSU parameters
+        quant.coverage <- ExtDist::qJohnsonSU(coverage, params = jparms.optim)
+        quant.parms <- c(quant=quant.coverage, jparms.optim[2:4])
+        out.optim.quant <- optim(par = quant.parms, # parameters to be optimized and initial guess
+                                 fn  = nll.johnsonsu.quant,     # NLL function
+                                 x   = x,                       # data
+                                 coverage   = 0.99,             # percent coverage
+                                 control = list(parscale = quant.parms), # not clear this was needed
+                                 hessian = TRUE
+                                 )
+        estimates      <- as.numeric(out.optim.quant$par)
+        standard.error <- as.numeric( sqrt(diag(solve(out.optim.quant$hessian))) )
+        coef           <- data.frame(estimates, standard.error)
+        rownames(coef) <- names(out.optim.quant$par)
+
+        jparms.optim.quant <- as.list(out.optim.quant$par)
+
+        ## write equivalent Johnson SU parameters in terms of gamma
+        gamma <- qnorm(coverage) - jparms.optim.quant$delta *
+            asinh( (jparms.optim.quant$quant-jparms.optim.quant$xi)/jparms.optim.quant$lambda )
+        jparms.q <- list(gamma  = gamma,
+                         delta  = jparms.optim.quant$delta,
+                         xi     = jparms.optim.quant$xi,
+                         lambda = jparms.optim.quant$lambda,
+                         type   = 'SU',
+                         quant  = jparms.optim.quant$quant)
         
+        ##----------------------
+        ## calculate the tolerance limit
+        ## factor <- qnorm(1 - alpha/side)   # alpha = 0.01, sided=1, z = 2.326
+        ## dof    <- length(x) - 4           # 4 independent fitting parameters in Johnson SU
+        ## student.t <- qt(1 - alpha/side, dof)  # 2.3326 for dof=598
+        ## factor <- student.t * sqrt(1 + 1/dof) # 2.3345
+        ## factor <- ExtDist::qJohnsonSU(1 - alpha/side, params = jparms.q[1:5])
+        ## 
+        ##         jparms.dan <- list(gamma  = 3.0202,
+        ##                            delta  = 1.4506,
+        ##                            xi     = 1.0148,
+        ##                            lambda = 00531,
+        ##                            type   = 'SU')
+        ##         factor <- ExtDist::qJohnsonSU(1 - alpha/side, params = jparms.dan) # 38
+        ## 
+        ## factor <- 2.6645    # not sure where 2.6645 comes from
+        ## xtol_lower <- jparms.q$quant - factor * standard.error[1]
+        ## xtol_upper <- jparms.q$quant + factor * standard.error[1]
+
+        ## following approach here:
+        ## https://personal.psu.edu/abs12/stat504/Lecture/lec3_4up.pdf
+
+        if (isTRUE(plot.ll)) {
+            ## plot the likelihood as a function of the quantile
+            quant.min <- estimates[1] - 2 * standard.error[1]
+            quant.max <- estimates[1] + 2 * standard.error[1]
+            quant     <- seq(quant.min, quant.max, length.out=100)
+            ll <- NA
+            for (i in 1:100) {
+                ## vary quant parameter to see impact on likelihood
+                jparms.vary.q <- list(quant  = quant[i],
+                                      delta  = jparms.optim.quant$delta,
+                                      xi     = jparms.optim.quant$xi,
+                                      lambda = jparms.optim.quant$lambda)
+                ll[i] <- -nll.johnsonsu.quant(x, coverage, jparms=jparms.vary.q, debug=FALSE)
+            }
+            plot(quant, ll, xlab='quantile', ylab='Log Likelihood')
+            abline(v=estimates[1])
+        }
+
+        ## find the likelihood associated with the best estimate
+        ll.max <- -nll.johnsonsu.quant(x,
+                                       coverage,
+                                       jparms=list(quant = estimates[1],
+                                                   delta  = jparms.optim.quant$delta,
+                                                   xi     = jparms.optim.quant$xi,
+                                                   lambda = jparms.optim.quant$lambda),
+                                       debug=FALSE)
+
+        ## confidene limit is defined at likelihood that is lower than max by chi-squared
+        ll.tol <-  ll.max - qchisq(1 - alpha/side, 1)   # qchisq(1-0.01/1, 1) = 6.634897 
+        if (isTRUE(plot.ll)) {
+            abline(h=ll.tol)
+        }
+
+        ## find the quantiles associaed with the likelihood tolerance
+        zero <- function(x0, data, coverage, delta, xi, lambda, ll.tol) {
+            ll <- -nll.johnsonsu.quant(data,
+                                       coverage,
+                                       jparms=list(quant  = x0,
+                                                   delta  = delta,
+                                                   xi     = xi, 
+                                                   lambda = lambda),
+                                       debug=FALSE)
+            zero <- ll - ll.tol
+        } 
+        out.nrl <- newton.raphson(f = zero,
+                                  data = x,
+                                  coverage = coverage,
+                                  delta  = jparms.optim.quant$delta,
+                                  xi     = jparms.optim.quant$xi,
+                                  lambda = jparms.optim.quant$lambda,
+                                  ll.tol = ll.tol,
+                                  xguess = estimates[1] - standard.error[1],  # xguess is used for 1st param in f
+                                  tol = 1e-5, n = 1000, plot='no')
+        quant.tol.lower <- out.nrl$root
+        out.nru <- newton.raphson(f = zero,
+                                  data = x,
+                                  coverage = coverage,
+                                  delta  = jparms.optim.quant$delta,
+                                  xi     = jparms.optim.quant$xi,
+                                  lambda = jparms.optim.quant$lambda,
+                                  ll.tol = ll.tol,
+                                  xguess = estimates[1] + standard.error[1],  # xguess is used for 1st param in f
+                                  tol = 1e-5, n = 1000, plot='no')
+        quant.tol.upper <- out.nru$root
+        if (isTRUE(plot.ll)) {
+            points(estimates[1]   , ll.max, col='blue', pch=16, cex=2)
+            points(quant.tol.lower, ll.tol, col='red' , pch=16, cex=2)
+            points(quant.tol.upper, ll.tol, col='red' , pch=16, cex=2)
+        }
+        
+        if (isTRUE(plots)) {
+            ## plot fit from R packages vs my MLE fit vs. my MLE fit on quantiles
+            plotspace(2,2)
+            hist(x, freq=FALSE, main='Johnson SU Fits', xlim=range(quant.tol.lower, x, quant.tol.upper))
+            curve(SuppDists::dJohnson(x, jparms)       , min(x, quant.tol.lower), max(x, quant.tol.upper),
+                  col='black', lty=1, add=TRUE)
+            curve(SuppDists::dJohnson(x, jparms.optim) , min(x, quant.tol.lower), max(x, quant.tol.upper),
+                  col='red', lty=1, add=TRUE, lwd=2)
+            curve(SuppDists::dJohnson(x, jparms.q[1:5]), min(x, quant.tol.lower), max(x, quant.tol.upper),
+                  col='blue', lty=2, add=TRUE)
+            abline(v=quant.tol.upper, col='red', lwd=2)
+            legend("topright", c("SuppDists or ExtDist", "MLE", "MLE fit on quantile", "upper tolerance limit"),
+                   col=c("black", "red", "blue", 'red'), lty=c(1,1,2,2))
+            qqplot_nwj(x, type='j', jfit=jparms       , main='ExtDist or SuppDists auto fit')
+            qqplot_nwj(x, type='j', jfit=jparms.optim , main='MLE fit with optim')
+            qqplot_nwj(x, type='j', jfit=jparms.q[1:5], main='MLE fit with optim on quantile')
+            
+        }
+
     }
-    
-    return(list(sided=side, 
-                alpha=alpha,
-                P=P,
-                jfit=jfit,
-                jparms=jparms,
-                xn=xn,
-                xntol_lower=xntol_lower,
-                xntol_upper=xntol_upper,
-                x=x,
-                xtol_lower=xtol_lower, 
-                xtol_upper=xtol_upper))
+
+    return(list(side         = side, 
+                alpha        = alpha,
+                coverage     = coverage,
+                jfit         = jfit,
+                jparms       = jparms,
+                jparms.optim = jparms.optim,
+                jparms.q     = jparms.q,
+                x            = x,
+                xtol_lower   = quant.tol.lower, 
+                xtol_upper   = quant.tol.upper))
 }
 
 ## ## test
@@ -92,34 +291,5 @@ johnson_tol <- function(x, jfit='all', alpha=0.01, P=0.99, side=1, plots=FALSE, 
 
 ## johnson_tol(mtcars$mpg, plots=TRUE)
 
-x <- mtcars$mpg
-
-## find best fit JohnsonSU paramters
-jparms.out <- ExtDist::eJohnsonSU(x)  # this failed
-jparms <- list(gamma   = jparms.out$gamma,
-               delta   = jparms.out$delta,
-               xi      = jparms.out$xi,
-               lambda  = jparms.out$lambda,
-               type    = 'SU')
-jparms  <- SuppDists::JohnsonFit(x) # returns SU
-gamma <- jparms$gamma
-delta <- jparms$delta
-xi    <- jparms$xi
-lambda <- jparms$lambda
-
-## find Pth quantile for the best fit distribution 
-## P = coverage (e.g., 99%)
-P <- 0.99
-quantile.P <- ExtDist::qJohnsonSU(P, params=jparms)
-
-## find Z assocaited with some % confidence on standard normal
-alpha <- 0.01
-sided <- 2
-z.conf <- qnorm(1 - alpha/sided)   # confidence = 1 - alpha; z.conf = 2.575829 for 1-sided 99
-
-## replace gamma 
-gamma.P.alpha <- z.conf - delta * asinh( (quantile.P - xi)/ lambda )
-
-## refit the Johnson distributions
-jparms  <- SuppDists::JohnsonFit(gamma.P.alpha) # returns SU
-
+## x <- mtcars$mpg
+## johnson_tol(x, jfit='all', alpha=0.01, P=0.99, side=1, plots=FALSE, breaks=NULL)
