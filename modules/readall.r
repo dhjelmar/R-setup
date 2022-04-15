d8587ad13cfc2b4a35c0aab463748e79bcf288db
dlhjel <dlhjel@gmail.com>
2022-04-15 14:57:10 -0400

    added control for reading NA

--- modules/readall.r
+++ modules/readall.r
@@ -1,12 +1,13 @@
 readall <- function(datasource,
-                    header.row=1,        # row number of the header; header.row=0 means no header
-                    data.start.row=NULL, # row where data starts; default is header.row + 1
-                    data.end.row=NULL,   # Excel only: row where data ends; default is end of file
-                    sheet=NULL,          # Excel only: sheet name in quotes; defaults to 1st sheet
-                    range=NULL,          # Excel only: cell range in quotes including 1 line header; overrides header.row and data.start.row
-                    col.names=NULL,      # Used to replace column names
-                    rename=TRUE,         # If true, change special characters and spaces in read names to "_";  not used if col.names is specified
-                    suppress=FALSE       # default prints output of variable names; set to true to suppress output
+                    header.row=1,         # row number of the header; header.row=0 means no header
+                    data.start.row=NULL,  # row where data starts; default is header.row + 1
+                    data.end.row=NULL,    # Excel only: row where data ends; default is end of file
+                    sheet=NULL,           # Excel only: sheet name in quotes; defaults to 1st sheet
+                    range=NULL,           # Excel only: cell range in quotes including 1 line header; overrides header.row and data.start.row
+                    col.names=NULL,       # Used to replace column names
+                    rename=TRUE,          # If true, change special characters and spaces in read names to "_";  not used if col.names is specified
+                    na=c('NA', 'na', ''), # strings to read as NA rather than character
+                    suppress=FALSE        # default prints output of variable names; set to true to suppress output
                     ) {
 
     ## e.g.,: readall("mydata.xlsx", sheet="Test Geometry", range="b3:x99")
@@ -25,17 +26,17 @@ readall <- function(datasource,
     ext <- substr(datasource, lastdot+1, nchar(datasource))
 
     if (lastone == '\n') {
-        df <- read.table(text=datasource, header=TRUE, stringsAsFactors=FALSE)
+        df <- read.table(text=datasource, header=TRUE, stringsAsFactors=FALSE, na.strings=na)
         datasourcename <- 'internal table'
     } else if (ext == 'csv' | ext == 'txt') {
         if (header.row == 0) {
             ## read data; no header
-            df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1, stringsAsFactors=FALSE)
+            df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1, stringsAsFactors=FALSE, na.strings=na)
         } else {
             ## read header
-            hd <- read.table(datasource, header=TRUE,  sep=",", skip=header.row-1, stringsAsFactors=FALSE)
+            hd <- read.table(datasource, header=TRUE,  sep=",", skip=header.row-1, stringsAsFactors=FALSE, na.strings=na)
             ## read data
-            df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1, stringsAsFactors=FALSE)
+            df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1, stringsAsFactors=FALSE, na.strings=na)
             ## assign header names to columns
             names(df) <- colnames(hd)
         }
@@ -46,10 +47,10 @@ readall <- function(datasource,
             ## range is defined so this takes precedence
             if (header.row !=0) {
                 ## simple structure with a single header row followed by data in the provided range
-                df <- read_excel(datasource, sheet=sheet, col_names=TRUE,  range=range)
+                df <- read_excel(datasource, sheet=sheet, col_names=TRUE,  range=range, na=na)
             } else {
                 ## no header provided
-                df <- read_excel(datasource, sheet=sheet, col_names=FALSE, range=range)
+                df <- read_excel(datasource, sheet=sheet, col_names=FALSE, range=range, na=na)
             }
 
         } else {
@@ -64,30 +65,30 @@ readall <- function(datasource,
                 ## no header row supplied
                 if (is.null(n_max)) {
                     ## read to end of file
-                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1)
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, na=na)
                 } else {
                     ## read specified number of rows
-                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, n_max=n_max)
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, n_max=n_max, na=na)
                 }                    
             } else if (data.start.row == header.row + 1) {
                 ## data immediately follows single row header so read both together
                 if (is.null(n_max)) {
                     ## read to end of file
-                    df <- read_excel(datasource, sheet=sheet, col_names=TRUE, skip=header.row-1)
+                    df <- read_excel(datasource, sheet=sheet, col_names=TRUE, skip=header.row-1, na=na)
                 } else {
                     ## add 1 to n_max to read header plus specified number of data rows
-                    df <- read_excel(datasource, sheet=sheet, col_names=TRUE, skip=header.row-1, n_max=n_max+1)
+                    df <- read_excel(datasource, sheet=sheet, col_names=TRUE, skip=header.row-1, n_max=n_max+1, na=na)
                 }
             } else {
                 ## lines between header row and data
                 ## read header
-                hd <- read_excel(datasource, sheet=sheet, col_names=TRUE,  skip=header.row-1, n_max=1)
+                hd <- read_excel(datasource, sheet=sheet, col_names=TRUE,  skip=header.row-1, n_max=1, na=na)
                 if (is.null(n_max)) {
                     ## read data to end of file
-                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1)
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, na=na)
                 } else {
                     ## read specified number of data rows
-                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, n_max=n_max)
+                    df <- read_excel(datasource, sheet=sheet, col_names=FALSE, skip=data.start.row-1, n_max=n_max, na=na)
                 }                    
                 ## assign header names to columns (added [] to following to strip off blank columns with header names)
                 names(df) <- colnames(hd)[1:length(names(df))]
