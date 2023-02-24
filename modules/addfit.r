58f2c7a3138d0db771795eb74d25438b0777bb34
dhjelmar <dlhjel@gmail.com>
2023-02-24 13:25:04 -0500

    added predictions to output

--- modules/addfit.r
+++ modules/addfit.r
@@ -27,14 +27,17 @@ addfit <- function(xx, yy, col='black', interval='conf', alpha=0.05, sided=2, pc
     }
     if (interval == 'mean') {
         lines(new.xx, slope * new.xx + intercept, lwd=2, col=col)
+        pred <- NA
     } else if (interval == 'line') {
         newdf <- data.frame(xx,yy)
         newdf <- newdf[order(newdf$xx),]
         xx <- newdf$xx
         yy <- newdf$yy
-        lines(xx, yy, lwd=2, col=col)       
+        lines(xx, yy, lwd=2, col=col)
+        pred <- NA
     } else {
-        pred   <- predict(fit, new=data.frame(xx=new.xx), interval=interval, level=1-alpha/sided)
+        pred <- predict(fit, new=data.frame(xx=new.xx), interval=interval, level=1-alpha/sided)
+        pred <- cbind(new.xx, pred)
         lines(new.xx, pred[,"fit"], lwd=2, col=col)
         lines(new.xx, pred[,"lwr"], lty=3, col=col)
         lines(new.xx, pred[,"upr"], lty=3, col=col)
@@ -52,6 +55,6 @@ addfit <- function(xx, yy, col='black', interval='conf', alpha=0.05, sided=2, pc
     }        
     equation = paste0("y = ", signif(intercept,4), " + ", signif(slope,4), " * x; ",
                       expression(Delta), " = ", signif(rise,4))
-    return(list(equation=equation, slope=slope, intercept=intercept, rise=rise))
+    return(list(equation=equation, slope=slope, intercept=intercept, rise=rise, pred=pred))
 
 }
