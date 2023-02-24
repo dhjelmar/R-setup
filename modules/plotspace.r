ee2120e4c25225867e2e111c470b000f44079ec2
dhjelmar <dlhjel@gmail.com>
2023-02-24 10:23:05 -0500

    added capability to automatically determine best layout based on number of plots needed

--- modules/plotspace.r
+++ modules/plotspace.r
@@ -1,8 +1,38 @@
-plotspace <- function(rows,columns) par(mfrow=c(rows,columns))
+plotspace <- function(rows, columns, nplots=NA) {
+    ## input: rows    = number of plots per row
+    ##        columns = number of plots per column
+    ##        nplots  = NA (default) ignors this input
+    ##                = number overwrites any specificed number of rows and columns
+    ##                  if provided to instead best fit all plots
 
-#----------------------------------------------------------------------------
+    if (!is.na(nplots)) {
+        ## figure out the best arrangement for plots
+        rows    <- ceiling(nplots^0.5)
+        columns <- ceiling(nplots / rows)
+    }
 
-# printdf(df,nrows,var) where var is a vector of variable names.
-# var is a vector list of all variables in dataframe, df, to be printed.
-# Each variable needs to be in quotes, e.g.:
-#     printdf(df,18,c('ID','Work Item Type','Effort'))
+    ## set number of rows and columns
+    par(mfrow=c(rows,columns))
+}
+
+## plotspace(2,2)
+## plot(mtcars$cyl , mtcars$mpg)
+## plot(mtcars$disp, mtcars$mpg)
+## plot(mtcars$wt  , mtcars$mpg)
+## 
+## plotspace(nplots=11)
+## plot(mtcars$cyl , mtcars$mpg)
+## plot(mtcars$disp, mtcars$mpg)
+## plot(mtcars$wt  , mtcars$mpg)
+## plot(mtcars$gear, mtcars$mpg)
+## plot(mtcars$carb, mtcars$mpg)
+## plot(mtcars$hp  , mtcars$mpg)
+## 
+## plotspace(2,2,nplots=5)
+## plot(mtcars$cyl , mtcars$mpg)
+## plot(mtcars$disp, mtcars$mpg)
+## plot(mtcars$wt  , mtcars$mpg)
+## plot(mtcars$gear, mtcars$mpg)
+## plot(mtcars$carb, mtcars$mpg)
+## plot(mtcars$hp  , mtcars$mpg)
+
