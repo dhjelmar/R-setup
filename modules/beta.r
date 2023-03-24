d12c1624ef23e005daafc7bd1ca7bae93d9c075a
dhjelmar <dlhjel@gmail.com>
2023-03-24 11:21:04 -0400

    changed to more descriptive function name

--- modules/beta.r
+++ modules/beta.r
@@ -1,21 +1,21 @@
-## calculate beta (1st moment of inertia) for piecewise linear function y = f(x)
-beta <- function(x, y) {
+## calculate moi (1st moment of inertia) for piecewise linear function y = f(x)
+moi <- function(x, y) {
 
-    beta <- 0
+    moi <- 0
     for (i in 2:length(x)) {
         ## for each segment
 
         if (x[i] != x[i-1]) {
-            ## not a step change so segment will add to beta for segment of length base
+            ## not a step change so segment will add to moi for segment of length base
             base      <-  x[i] - x[i-1]
 
-            ## calculate beta for rectangular part
+            ## calculate moi for rectangular part
             rheight   <- min(y[i], y[i-1])
             rarea     <- base * rheight
             rcentroid <- (x[i] + x[i-1]) / 2
-            beta      <- beta + rarea * rcentroid
+            moi      <- moi + rarea * rcentroid
 
-            ## calculate additional beta for triangular part, if any
+            ## calculate additional moi for triangular part, if any
             if (y[i-1] != y[i]) {
                 ## there is additional area to consider
                 theight <- max(y[i], y[i-1]) - rheight
@@ -27,13 +27,13 @@ beta <- function(x, y) {
                     ## triangle height decreases with distance
                     tcentroid <- x[i-1] + base * 1 / 3
                 }
-                beta <- beta + tarea * tcentroid
+                moi <- moi + tarea * tcentroid
             }
         }
     }
-    return(beta)
+    return(moi)
 }    
 
 ## x <- c(0, 10, 10, 15, 20)
 ## y <- c(5,  5, 10, 10,  5)
-## beta(x,y)
+## moi(x,y)
