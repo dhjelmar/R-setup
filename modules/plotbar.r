eaa0b3b0fd7f5e87f13eb23ccbf887530a1e7206
dhjelmar <dlhjel@gmail.com>
2023-11-10 15:04:57 -0500

    rewritten to be a useful function rather than an example

--- modules/plotbar.r
+++ modules/plotbar.r
@@ -1,32 +1,38 @@
-plotbar <- function(seed, xnum, xcennum) {
+plotbar <- function(y, ylow, yhigh, ylab='y', sorty=TRUE) {
+  ## Given values y with low and high values representing uncertainty
+  ## Plot the y values as open circles and add lines connecting low and high values
+
+  ## put data into dataframe and sort if needed
+  df <- data.frame(y=y, ylow=ylow, yhigh=yhigh)
+  if (isTRUE(sorty)) df <- df[order(df$y),]
+  df$index <- c(1:nrow(df))
+
+  ## plot all points
+  plot(df$index, df$y, ylim=range(ylow, yhigh), xlab='Index', ylab=ylab)
   
-  ## generate data
-  set.seed(seed)
-  x <- rnorm(xnum)
-  x <- sort(x)
-  xid <- round(runif(xcennum, 1, xnum))
-  df1 <- data.frame(x.low=x, x.high=x, type='known')
-  x.low  <- x[xid] * 0.50
-  x.high <- x[xid] * 1.50
-  df2 <- data.frame(x.low=x.low, x.high=x.high, type='censored')
-  df <- rbind(df1, df2)
-  df$x.med <- (df$x.low + df$x.high)/2
+  ## plot known points
+  known <- which(df$ylow==df$yhigh)
+  points(df$index[known], df$y[known], pch=16)
   
-  ## create plot
-  df <- df[order(df$x.med),]
-  plot(df$x.med, ylim=range(df$x.low, df$x.high))
-  lines(df$x.low)
-  lines(df$x.high)
+  ## add "error bar" lines
+  lines(df$index, df$ylow)
+  lines(df$index, df$yhigh)
+  
+  ## add legend
+  legend('topleft', 
+         legend=c('Censored (lines for range)', 'Known'), 
+         pch=c(1,16))
+  
   return(df)
 }
 
-  plotspace(3,1)
-  out <- plotbar(1, 10, 3)
-  out <- plotbar(1, 100, 3)
-  out <- plotbar(1, 1000, 3)
-
-
-## Example of how to plot a dataset with error bars
 plotbar_run <- function() {
-  out[out$type=="censored",]
+  plotspace(2,2)
+  ylow  <- c(1,6,2,2.5,3.8,5)
+  y     <- c(1,6,2,4,5,5)
+  yhigh <- c(1,6,2,5,6,5)
+  plotbar(y=y, ylow=ylow, yhigh=yhigh, sorty=NA)
+  plotbar(y=y, ylow=ylow, yhigh=yhigh)
+  plotbar(mtcars$mpg, mtcars$mpg*0.8, mtcars$mpg*1.2, 'mpg from mtcars database', sorty=NA)
+  plotbar(mtcars$mpg, mtcars$mpg*0.8, mtcars$mpg*1.2, 'mpg from mtcars database')
 }
