3c8ecbfcc28eabe9715fff2970a6516721c75977
dhjelmar <dlhjel@gmail.com>
2024-06-11 09:52:15 -0400

    test for internal table was failing, so changed logic to default to assuming source data is an internal table

--- modules/readall.r
+++ modules/readall.r
@@ -6,7 +6,7 @@ readall <- function(datasource,
                     range=NULL,           # Excel only: cell range in quotes including 1 line header; overrides header.row and data.start.row
                     col.names=NULL,       # Used to replace column names
                     rename=TRUE,          # If true, change special characters and spaces in read names to "_";  not used if col.names is specified
-                    na=c('NA', 'na', 'N/A', 'n/a', 'NaN', 'nan', ''), # strings to read as NA rather than character
+                    na=c(NA, 'NA', 'na', 'N/A', 'n/a', 'NaN', 'nan', ''), # strings to read as NA rather than character
                     suppress=FALSE        # default prints output of variable names; set to true to suppress output
                     ) {
 
@@ -17,24 +17,18 @@ readall <- function(datasource,
     
     ## datasource <- "test.xlsx"
 
-    ## determine if datasource is actually a data table rather than a file
-    lastone <- substr(datasource, nchar(datasource), nchar(datasource))
-            
     ## find extension of the datasource
     ## find a dot that is followed by anything but a dot until the string ends
     lastdot  <- regexpr("\\.[^\\.]*$", datasource)[1]
     ext <- substr(datasource, lastdot+1, nchar(datasource))
 
-    if (lastone == '\n') {
-        df <- read.table(text=datasource, header=TRUE, stringsAsFactors=FALSE, na.strings=na)
-        datasourcename <- 'internal table'
-    } else if (ext == 'csv' | ext == 'txt') {
+    if (ext == 'csv' | ext == 'txt') {
         if (header.row == 0) {
             ## read data; no header
             df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1, stringsAsFactors=FALSE, na.strings=na)
         } else {
             ## read header
-            hd <- read.table(datasource, header=TRUE,  sep=",", skip=header.row-1, stringsAsFactors=FALSE, na.strings=na, check.names=FALSE)
+            hd <- read.table(datasource, header=TRUE,  sep=",", skip=header.row-1, nrows=1, stringsAsFactors=FALSE, na.strings=na, check.names=FALSE)
             ## read data
             df <- read.table(datasource, header=FALSE, sep=",", skip=data.start.row-1, stringsAsFactors=FALSE, na.strings=na)
             ## assign header names to columns
@@ -98,9 +92,11 @@ readall <- function(datasource,
         }
         datasourcename <- datasource
     } else {
-        datasourcename <- datasource
-        cat('FATAL ERROR: unknown datasource type\n')
-        stop()
+        ## assume data source is an internal table
+        df <- read.table(text=datasource, header=TRUE, stringsAsFactors=FALSE, na.strings=na)
+        datasourcename <- 'internal table'
+        ## cat('FATAL ERROR: unknown datasource type\n')
+        ## stop()
     }
     
     if (suppress == FALSE) {
@@ -127,16 +123,20 @@ readall <- function(datasource,
     return(df)
 }
 
-## following is read in as a string where the last two characters are \n
-## use above info to identify this as data table rather than external file
-## mydata <- "
-## num     x1      x2      x3      x4      x5      x6
-## 1 	55 	10 	5 	1580 	17.5 	20 
-## 2 	75 	10 	5 	1580 	29 	26 
-## 3 	55 	25 	5 	1580 	29 	20 
-## "
-## df <- read.table(text=mydata, header=TRUE)
-## df <- readall(mydata, suppress=TRUE)
-## df <- readall('test.xlsx', header.row=2, data.end.row=5)
-## df <- readall('test.xlsx', range='b2:d11')
-## readall('readall.xlsx', range='b2:e5', col.names=c('fred', 'Ethyl Mertz', 'lucy', 'desi'))
+readall_test <- function() {
+  ## following is read in as a string where the last two characters are \n
+  ## use above info to identify this as data table rather than external file
+   mydata <- "
+   num     x1      x2      x3      x4      x5      x6
+   1 	55 	10 	5 	1580 	17.5 	20 
+   2 	75 	NA 	na 	1580 	29 	26 
+   3 	55 	25 	5 	NaN 	n/a	20
+   "
+   df <- read.table(text=mydata, header=TRUE, na.strings=c(NA, 'NA', 'na', 'NaN', 'n/a'))
+   print(df)
+   df <- readall(mydata, suppress=TRUE)
+   df <- readall('test.xlsx', header.row=2, data.end.row=5)
+   df <- readall('test.xlsx', range='b2:d11')
+   df <- readall('readall.xlsx', range='b2:e5', col.names=c('fred', 'Ethyl Mertz', 'lucy', 'desi'))
+   df
+}
