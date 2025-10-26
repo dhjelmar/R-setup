2fd1cc87d76751569c9c96614dcba1828e7ad56f
dhjelmar <dlhjel@gmail.com>
2025-10-25 22:26:36 -0400

    completed 2-sided vs. 1-sided tolerance example

--- tolerance_limit_example.r
+++ tolerance_limit_example.r
@@ -29,47 +29,52 @@ out_non$message
 out_non$df
 
 ################################################################################
-
 # 1-sided vs. 2-sided tolerance bounds
+
 set.seed(100)
 x <- rnorm(1000,mean=0,sd=1)
 
 ###########################
-# per looking online
 
-# 1-sided
-alpha <- 0.1
-conf <- 1 - alpha      # 0.90
-P <- 0.95
-tolerance::normtol.int(x, side=1, alpha=0.1, P=0.95)           # -1.744, 1.778
+# using tolerance factors (k)
+# tolerance factor table
+# https://ntrs.nasa.gov/api/citations/19670023646/downloads/19670023646.pdf
+k <- 4.383    # for n=25, conf=0.95, and P=0.99
+k <- 2.719    # n=1000, conf=0.99, P (reliabiilty) = 0.99
+k <- 2.036    # n=1000, conf=0.95, P (reliabiilty) = 0.95
+lower_tol_limit = mean(x) - k * sd(x) 
+upper_tol_limit = mean(x) + k * sd(x)
+lower_tol_limit                                 # -2.081
+upper_tol_limit                                 #  2.115
 
-# 2-sided
-# conf = 1 - alpha_1_sided / 2
-alpha <- (1 - conf)*2  # 0.2
-P <- (1+P)/2           # 0.975
-tolerance::normtol.int(x, side=2, alpha=0.2, P=0.975)          # -2.34, 2.37
+# my 2-sided MLE function gives a very 2-sided bounds to the above
+mle.normal.tol(x, sided=2, conf=0.95, P=0.95)   # -2.115, 2.148
+out <- hist_nwj(x, type='n', sided=2, conf=0.95, P=0.95) 
+out$tol_out_norm$tolerance                      # -2.115, 2.148
 
+# using my MLE functions for 1-sided with same conf and P not as wide as 2-sided bounds
+mle.normal.tol(x, side='lower', sided=1, conf=0.95, P=0.05)  # -1.762  not as low
+mle.normal.tol(x, side='upper', sided=1, conf=0.95, P=0.95)  #  1.780  not as high
 
-###########################
-# upper, 1-sided
-out1U <- hist_nwj(x, type='n', side='upper', sided=1, conf=0.90, P=0.95)     #  1.776
-mle.normal.tol(   x,           side='upper', sided=1, conf=0.90, P=0.95)     
+# what needs to be done to use 1-sided bounds to match 2-sided bounds?
+# 2-sided alpha above was 0.05
+#     conf= 1-alpha/sided = 1 - 0.05/2 = 0.975
+# 2-sided coverage (P) was 95%, so missing 0.025 on the low side and 0.025 on the high side
+#     1-sided, lower P = 0.025 
+#     1-sided, upper P = 1 - 0.025 = 0.975
+mle.normal.tol(x, side='lower',  sided=1, conf=0.975, P=0.025)  # -2.115 <- close to 2-sided lower from NTRS
+mle.normal.tol(x, side='upper',  sided=1, conf=0.975, P=0.975)  #  2.148 <- close to 2-sided upper from NTRS
+out <- hist_nwj(x, type='n', side='lower', sided=1, conf=0.975, P=0.025) 
+out$tol_out_norm$tolerance                                     #  -2.115
+out <- hist_nwj(x, type='n', side='upper', sided=1, conf=0.975, P=0.975) 
+out$tol_out_norm$tolerance                                     #  2.148
 
-# lower, 1-sided
-out1L <- hist_nwj(x, type='n', side='lower', sided=1, conf=0.90, P=0.05)     # -1.743
-mle.normal.tol(   x,           side='lower', sided=1, conf=0.90, P=0.05)
-
-# 2-sided
-# P = 1 - (1-Pupper) - Plower = 0.90 rather since missing 0.05 on each side
-# But what to do with confidence (or alpha)?
-#
-# the following is needed to match the two 1-sided calculations
-out2  <- hist_nwj(x, type='n', side='both' , sided=2, conf=0.80, P=0.90)     # -1.743, 1.776
-mle.normal.tol(   x,                         sided=2, conf=0.80, P=0.90)
-mle.normal.tol(   x,                         sided=2, alpha=0.1*2, P=0.90)
-#
-# but should it be the following instead since now half the conf is on the low side and half is on the upper side?
-out2  <- hist_nwj(x, type='n', side='both' , sided=2, conf=0.95, P=0.90)     # -1.778, 1.812
-mle.normal.tol(   x,                         sided=2, alpha=0.1/2, P=0.90)
-mle.normal.tol(   x,                         sided=2, conf=0.95, P=0.90)
+# using standard R functions
+conf <- 0.95
+tolerance::normtol.int(x, side=2, alpha=  1 - conf        , P=0.95)   # -2.082, 2.115 <- almost identical to NTRS
+tolerance::normtol.int(x, side=1, alpha=  1 - conf        , P=0.95)   # -1.763, 1.797 <- not as wide as 2-sided bounds (same as found above)
+# what needs to be done to use 1-sided bounds to match 2-sided bounds?
+sided <- 2
+alpha <- (1-conf)*sided   # 0.1
+tolerance::normtol.int(x, side=1, alpha= (1 - conf) * sided, P=0.975) # -2.077, 2.110 <- close to above 2-sided
 
